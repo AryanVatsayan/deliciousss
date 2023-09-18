95de22aa2116c60686685d347e5ad56fe0cd8324
dashingvinit <21bcs7277@cuchd.in>
2023-09-18 11:33:52 +0530

    fixed css

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,54 +1,50 @@
-import { useEffect , useState} from "react";
-import styled from "styled-components";
-import {Splide,SplideSlide } from "@splidejs/react-splide";
-import"@splidejs/splide/dist/css/splide.min.css";
-import { Link } from "react-router-dom";
-
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/splide/dist/css/splide.min.css';
+import { Link } from 'react-router-dom';
 
 function Popular() {
-  const [Popular,setPopular] = useState([]);
+  const [Popular, setPopular] = useState([]);
 
-  useEffect (() => {
+  useEffect(() => {
     getPopular();
-  },[]);
-
+  }, []);
 
   const getPopular = async () => {
-      
-      const check = localStorage.getItem("popular");
-      if(check){
-        setPopular(JSON.parse(check));
-      }
-      else{
-        const api = await fetch(
-          `https://api.spoonacular.com/recipes/random?apiKey=da7128aaaafe4370a423068314444bfb&number=9`
-        );
-        const data = await api.json();
+    const check = localStorage.getItem('popular');
+    if (check) {
+      setPopular(JSON.parse(check));
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=da7128aaaafe4370a423068314444bfb&number=9`
+      );
+      const data = await api.json();
 
-        localStorage.setItem("popular", JSON.stringify(data.recipes));
-        setPopular(data.recipies);
-        console.log(data.recipes);
-      }
+      localStorage.setItem('popular', JSON.stringify(data.recipes));
+      setPopular(data.recipies);
+      console.log(data.recipes);
+    }
   };
 
-
   return (
-  <div>
-        <Wrapper>
-          <h3>Popular Picks</h3>
-          
-          <Splide options={{
+    <div>
+      <Wrapper>
+        <h3>Popular Picks</h3>
+
+        <Splide
+          options={{
             perPage: 4,
             arrows: false,
             pagination: false,
             drag: 'free',
             gap: '5rem',
           }}>
-          {Popular?.map((recipe)=>{
-            return(
+          {Popular?.map((recipe) => {
+            return (
               <SplideSlide key={recipe.id}>
                 <Card>
-                  <Link to={"/recipe/" + recipe.id}>
+                  <Link to={'/recipe/' + recipe.id}>
                     <p>{recipe.title}</p>
                     <img src={recipe.image} alt={recipe.title} />
                     <Gradient />
@@ -57,37 +53,35 @@ function Popular() {
               </SplideSlide>
             );
           })}
-          </Splide>
-        </Wrapper>
-
+        </Splide>
+      </Wrapper>
     </div>
   );
-  
 }
 
-  const Wrapper = styled.div`
-    margin: 4rem 0rem;
-  `;
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
 
-  const Card = styled.div`
-   min-height: 25rem;
-   boder-radius: 2rem;
-   overflow: hidden;
-   position: relative;
-
-   img{
-    boder-radius: 2rem;
+const Card = styled.div`
+  min-height: 15rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+  img {
+    border-radius: 2rem;
     position: absolute;
     left: 0;
     width: 100%;
     height: 100%;
     object-fit: cover;
-   }
-   p{
+  }
+  p {
     position: absolute;
-    zindex:10;
+    z-index: 10;
     left: 50%;
-    bottom: 0%;
+    bottom: 0;
+    transform: translate(-50%, 0%);
     color: white;
     width: 100%;
     text-align: center;
@@ -97,14 +91,14 @@ function Popular() {
     display: flex;
     justify-content: center;
     align-items: center;
-   }
-  `;
+  }
+`;
 
-  const Gradient = styled.div`
-   z-index:3;
-   position: absolute;
-   width: 100%;
-   height: 100%;
-   backgorund: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5));
-   `
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.5) 100%);
+`;
 export default Popular;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,46 +4,45 @@ import { FaSearch } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 
 function Search() {
-
-  const[input,setInput] = useState("");
+  const [input, setInput] = useState('');
   const navigate = useNavigate();
 
-  const submitHandler = (e) =>{
+  const submitHandler = (e) => {
     e.preventDefault();
-    navigate('/searched/' + input );
+    navigate('/searched/' + input);
   };
   return (
-    <FormStyle>
+    <FormStyled>
       <div>
-      <FaSearch></FaSearch>
+        <FaSearch></FaSearch>
         <input onChange={(e) => setInput} type="text" value={input} />
       </div>
-    </FormStyle>
-  )
+    </FormStyled>
+  );
 }
 
-const FormStyle = styled.form`
+const FormStyled = styled.form`
   margin: 0rem 20rem;
-  <div>
-  width: 100%;
+  div {
     position: relative;
-  </div>
-  input{
+    width: 100%;
+  }
+  input {
+    width: 100%;
     border: none;
-    background: linear-gradient(35deg, #494949, #313131);
+    background: linear-gradient(to right, #494949, #313131);
     font-size: 1.5rem;
     color: white;
     padding: 1rem 3rem;
-    border: none;
     border-radius: 1rem;
     outline: none;
-    width: 100%;
   }
-  svg{
+  svg {
     position: absolute;
     top: 50%;
     left: 0%;
-    transform:translate(100%, -50%);
+    transform: translate(100%, -50%);
     color: white;
-  }`
-export default Search
+  }
+`;
+export default Search;
